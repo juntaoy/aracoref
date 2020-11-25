9c014650f101ba429e7964a7c1c0e43ec77eacad
Juntao Yu <30804976+juntaoy@users.noreply.github.com>
2020-11-25 18:58:52 +0000

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
 
 # Download pretrained embeddings.
-#curl -O http://lsz-gpu-01.cs.washington.edu/resources/glove_50_300_2.txt
-#curl -O https://nlp.stanford.edu/data/glove.840B.300d.zip
-#unzip glove.840B.300d.zip
-#rm glove.840B.300d.zip
+curl -O https://dl.fbaipublicfiles.com/fasttext/vectors-crawl/cc.ar.300.vec.gz
+gunzip cc.ar.300.vec.gz
 
 # Build custom kernels.
 TF_CFLAGS=( $(python -c 'import tensorflow as tf; print(" ".join(tf.sysconfig.get_compile_flags()))') )
